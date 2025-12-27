2701601a58543f623603310484afd555d793c48f
Laith Alnajjar <laithalnajjar2004@gmail.com>
2025-12-27 13:07:06 +0300

    feat(types): add strict numeric enums matching backend

---
+++ frontend/src/types/enums.ts
@@ -0,0 +1,109 @@
+// Mapped from C# HumanOvaa.Enums
+
+export enum UserRole {
+  Student = 1,
+  Volunteer = 2,
+  Charity = 3,
+  Company = 4,
+  University = 5,
+  DisabledStudent = 6
+}
+
+export enum Gender {
+  Male = 1,
+  Female = 2
+}
+
+export enum AvailabilityType {
+  FullTime = 1,
+  PartTime = 2,
+  WeekendsOnly = 3
+}
+
+export enum StudyLevel {
+  Year1 = 1,
+  Year2 = 2,
+  Year3 = 3,
+  Year4 = 4,
+  Year5 = 5,
+  Year6 = 6,
+  Year7 = 7
+}
+
+export enum DisabilityType {
+  Visual = 1,
+  Hearing = 2,
+  Mobility = 3,
+  Learning = 4,
+  Speech = 5,
+  Other = 6,
+  Physical = 7
+}
+
+export enum AssociationField {
+  Educational = 1,
+  Health = 2,
+  Social = 3,
+  Relief = 4
+}
+
+export enum StudentSkill {
+  Communication = 1,
+  Teamwork = 2,
+  Leadership = 3,
+  TimeManagement = 4,
+  // Technical
+  Programming = 101,
+  WebDevelopment = 102,
+  DataAnalysis = 103,
+  UIUX = 104,
+  TechnicalSupport = 105,
+  Databases = 106,
+  CSharp = 107
+}
+
+export enum VolunteerSkill {
+  Communication = 1,
+  SocialWork = 2,
+  Logistics = 3,
+  TechnicalSupport = 4,
+  EventManagement = 5,
+  Teaching = 6,
+  FieldWork = 7,
+  Administration = 8,
+  Teamwork = 9
+}
+
+export enum AssistanceType {
+  Accompaniment = 1,
+  NoteTaking = 2,
+  Transportation = 3,
+  AcademicSupport = 4,
+  CampusEscort = 5,
+  TechnicalSupport = 6,
+  ExamAssistance = 7
+}
+
+export enum University {
+  // Public
+  UniversityOfJordan = 1,
+  YarmoukUniversity = 2,
+  MutahUniversity = 3,
+  JordanUniversityOfScienceAndTechnology = 4,
+  HashemiteUniversity = 5,
+  AlAlBaytUniversity = 6,
+  AlBalqaAppliedUniversity = 7,
+  AlHusseinBinTalalUniversity = 8,
+  TafilaTechnicalUniversity = 9,
+  GermanJordanianUniversity = 10,
+  // Private
+  AppliedSciencePrivateUniversity = 101,
+  AlAhliyyaAmmanUniversity = 102,
+  PhiladelphiaUniversity = 103,
+  IsraUniversity = 104,
+  PetraUniversity = 105,
+  AlZaytoonahUniversityOfJordan = 106,
+  MiddleEastUniversity = 107,
+  JadaraUniversity = 108,
+  AmmanArabUniversity = 109
+}
